e936101947397251983f8132f742963d17600237
Daniel.Lan <daniel.lan@hy-tech.com.tw>
2026-04-20 22:12:30 +0800

    更新

--- Formula/adt.rb
+++ Formula/adt.rb
@@ -1,5 +1,5 @@
-class adt < Formula
-     desc "你的程式描述"
+class Adt < Formula
+     desc "A cross-platform CLI for safely querying Oracle databases from AI agents (Claude Code, etc.)."
      homepage "https://github.com/nilm987521/adt"
      url "https://github.com/nilm987521/adt/releases/download/v0.0.1/adt_darwin_arm64.tar.gz"
      sha256 "a5aff081e335622eb11eb88a46b9ddd7a418fb553b96595f666f60c15a31ae02"
